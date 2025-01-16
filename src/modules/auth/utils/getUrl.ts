884dc1dc28d88116d8853754fb703e0d58b586df
Jose Castellano <jcastellanosierras@gmail.com>
2025-01-16 18:22:26 +0100

    Revert "fix(auth): set project url if the enviroment is production"
    This reverts commit 7bf597297ba0c22fb046d070153c068c11febf0b.

--- src/modules/auth/utils/getUrl.ts
+++ src/modules/auth/utils/getUrl.ts
@@ -1,34 +1,11 @@
-// export const getURL = () => {
-//   const isProduction = import.meta.env.PROD;
-//   const vercelURL = import.meta.env.VITE_VERCEL_URL; // URL generada en los entornos de Vercel (autoestablecida).
-//   const productionURL = import.meta.env.VITE_VERCEL_PROJECT_PRODUCTION_URL; // URL configurada para producción.
-
-//   if (isProduction) {
-//     // Si estás en producción o en preview.
-//     if (vercelURL) {
-//       // En un entorno de preview, Vercel establece VITE_VERCEL_URL.
-//       return `https://${vercelURL}`;
-//     }
-//     // Si no es preview, debe ser producción.
-//     return `https://${productionURL}`;
-//   }
-
-//   // Si no es producción, estás en desarrollo.
-//   const devURL = 'http://localhost:5173/';
-
-//   // Normaliza la URL.
-//   return devURL.endsWith('/') ? devURL : `${devURL}/`;
-// };
-
-
 export const getURL = () => {
   let url =
-    import.meta.env.VITE_VERCEL_URL ??
-    import.meta.env.VITE_VERCEL_PROJECT_PRODUCTION_URL ??
-    'http://localhost:5173/'
-    
+    import.meta.env.VITE_SITE_URL ?? // Set this to your site URL in production env.
+    import.meta.env.VITE_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/'
+  // Make sure to include `https://` when not localhost.
   url = url.startsWith('http') ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
   url = url.endsWith('/') ? url : `${url}/`
-
   return url
 }
