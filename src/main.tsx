b2ffdc167d642fc898909577c83f305d2f8297f7
Jose Castellano <jcastellanosierras@gmail.com>
2025-01-16 17:55:23 +0100

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import { AuthProvider } from './components/AuthProvider';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <AuthProvider>
       <App />
     </AuthProvider>
